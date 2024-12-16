2468ecbb981c7a1a09ee0d9edcac7d8a46914433
Luke Taylor <luke@bigring.org>
2024-12-15 19:50:23 -0500

    Reenable other pages apparently

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite"
+import { resolve } from "path"
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        canvas: resolve(__dirname, "./demo/canvas.html"),
+        credits: resolve(__dirname, "./demo/credits.html"),
+        expensive: resolve(__dirname, "./demo/expensive.html"),
+        grid: resolve(__dirname, "./demo/grid.html"),
+        old: resolve(__dirname, "./demo/old.html"),
+      },
+    },
+  },
+})
